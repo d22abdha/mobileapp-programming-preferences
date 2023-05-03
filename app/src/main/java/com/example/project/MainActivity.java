a7c060fa426878fabf71142d1c1ab821706aa4ee
Abdishakur <Dhagaxg@gmail.com>
2023-05-03 18:36:30 +0200

    Skapat second Activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView sharedOutput;
+    private final String key = String.valueOf(R.string.keyData);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
 }
